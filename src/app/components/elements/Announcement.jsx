47179cc0f60ed738c2b7ed824fdceda578e576ca
Quoc Huy Nguyen Dinh <quochuy@gmail.com>
2020-03-06 15:24:20 +1100

    Steemulant staging announcement

--- src/app/components/elements/Announcement.jsx
+++ src/app/components/elements/Announcement.jsx
@@ -4,8 +4,13 @@ import React from 'react';
 export const Announcement = ({ onClose }) => (
     <div className="annoucement-banner">
         <p className="announcement-banner__text">
-            Communities are live!{' '}
-            <a className="announcement-banner__link" href="https://steemit.com/steemit/@steemitblog/communities-have-arrived">Read more</a>.
+            Checkout beta features on
+            <a
+                className="announcement-banner__link"
+                href="https://staging.steemulant.com"
+            >
+                the staging site
+            </a>.
         </p>
         <button className="close-button" type="button" onClick={onClose}>
             &times;
