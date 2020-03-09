4089bce16a72165099e689b42dadb8d6ccdfcbfe
Quoc Huy Nguyen Dinh <quochuy@gmail.com>
2020-03-09 22:18:58 +1100

    Modify announcememt

--- src/app/components/elements/Announcement.jsx
+++ src/app/components/elements/Announcement.jsx
@@ -4,7 +4,10 @@ import React from 'react';
 export const Announcement = ({ onClose }) => (
     <div className="annoucement-banner">
         <p className="announcement-banner__text">
-            Checkout beta features on{' '}
+            Maintained by{' '}
+            <a href="https://wallet.steemulant.com/~witnesses">
+                Witness @quochuy
+            </a>. Try beta features on{' '}
             <a
                 className="announcement-banner__link"
                 href="https://staging.steemulant.com"
